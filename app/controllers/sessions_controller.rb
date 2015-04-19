cd40ff893fc26dcbd61d678de40af96d4cdf3626
Mario Vizcaino <mario.vizcainos@gmail.com>
2015-04-19 15:38:46 -0500

    arreglo al controlador

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,14 @@ class SessionsController < ApplicationController
   def create
   	 user = login(params[:email], params[:password],params[:remember_me])  
     if user  
-      redirect_to :root, :notice => "Session Iniciada Correctamente"  
+      redirect_to :root
     else  
-      flash.now.alert = "Email or password was invalid."
       redirect_to :root, :notice => "Clave Erronea"  
     end  
   end
 
   def destroy
   	logout  
-	redirect_to root_url, :notice => "Logged out!"
+  	redirect_to root_url, :notice => "Logged out!"
   end
 end
